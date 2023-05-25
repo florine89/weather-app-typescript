0d180363bc44c8e8801c1f9771fc6f3b29781e7d
florine89 <114070434+florine89@users.noreply.github.com>
2023-05-25 15:17:06 +0200

    connexionapiok

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,30 @@
 // My main container
 
-const App = () => {
+import { useState, ChangeEvent } from "react";
+
+const App = (): JSX.Element => {
+
+  //variable pour recherche la localisation dans forecast weather
+  const [term, setTerm] = useState<string>('')
+
+  const getSearchoptions = (value: string) => {
+   // je fais appel à mon api sur OpenWeather
+   fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${value.trim()}&limit=5&appid=${process.env.REACT_APP_API_KEY}`)
+  }
+  //http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+  
+
+  // je définis le type que je veux en changeant mon usestate avec nouvelle valeur (setTerm)
+  const InputChange = (e: ChangeEvent<HTMLInputElement>) => {
+
+    const value = e.target.value.trim()
+    setTerm(value)
+
+    if (value === '' ) return
+
+    getSearchoptions(value)
+  }
+
   return (
     <main className="flex justify-center items-center bg-gradient-to-br from-green-400 via-rose-400
      to-yellow-300 h-[100vh] w-full">
@@ -12,11 +36,13 @@ const App = () => {
           Enter below a place you want to know the weather of and select an option from dropdown
         </p>
         <div className="flex mt-10 md:mt-4">
-        <input type="text" value={''} className=" border-2 rounded-l-md mt-3 border-white"/>
-        <button className="rounder-r-md border-2 border-white cursor-pointer
+          <input type="text" value={term}
+            onChange={InputChange}
+            className=" border-2 rounded-l-md mt-3 border-white" />
+          <button className="rounder-r-md border-2 border-white cursor-pointer
          hover:border-gray-700 text-gray-900 py-1 px-4 mt-3">
-Search
-        </button>
+            Search
+          </button>
         </div>
       </section>
 
