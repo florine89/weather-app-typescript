9bf62139ab3c86e29c39a9df65c1052351e4cff2
florine89 <114070434+florine89@users.noreply.github.com>
2023-05-25 21:34:29 +0200

    optionselectok

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,28 @@
 // My main container
 
 import { useState, ChangeEvent } from "react";
+import{ optionType } from './types'
 
 const App = (): JSX.Element => {
 
   //variable pour recherche la localisation dans forecast weather
   const [term, setTerm] = useState<string>('')
 
+  //variable pour afficher les villes que mon api va proposer avec la limite de 5 maximum
+  const [options, setOptions] = useState<[]>([])
+
+  // ma variable qui gère mon appel à l'api
   const getSearchoptions = (value: string) => {
-   // je fais appel à mon api sur OpenWeather
-   fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${value.trim()}&limit=5&appid=${process.env.REACT_APP_API_KEY}`)
+    // je fais appel à mon api sur OpenWeather
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${value.trim()}&limit=5&appid=${process.env.REACT_APP_API_KEY}`
+    ).then(res => res.json())
+      .then((data) => setOptions(data))
   }
   //http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
-  
+
+  const onOptionSelect = (option: optionType) => {
+console.log(option.name)
+  }
 
   // je définis le type que je veux en changeant mon usestate avec nouvelle valeur (setTerm)
   const InputChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -20,7 +30,7 @@ const App = (): JSX.Element => {
     const value = e.target.value.trim()
     setTerm(value)
 
-    if (value === '' ) return
+    if (value === '') return
 
     getSearchoptions(value)
   }
@@ -35,14 +45,33 @@ const App = (): JSX.Element => {
         <p className="text-sm mt-2">
           Enter below a place you want to know the weather of and select an option from dropdown
         </p>
-        <div className="flex mt-10 md:mt-4">
-          <input type="text" value={term}
+
+        <div className="relative flex mt-10 md:mt-4">
+          <input
+            type="text"
+            value={term}
             onChange={InputChange}
-            className=" border-2 rounded-l-md mt-3 border-white" />
-          <button className="rounder-r-md border-2 border-white cursor-pointer
-         hover:border-gray-700 text-gray-900 py-1 px-4 mt-3">
+            className=" px-2 py-1 rounded-l-md border-2 border-white" />
+
+          <button className="rounder-r-md border-2 border-zink-100
+          hover:border-zink-500 hover:text-zinc-500 text-zink-100
+          px-2 py-2 cursor-pointer">
             Search
           </button>
+
+          <ul className="absolute top-9 bg-white rounded-b-md">
+            {options.map((option: optionType, index: number) => (
+              <li key={option.name + '-' + index}>
+                <button className="text-left text-sm w-full hover:bg-zinc-700
+              hover:text-white px-2 py-1 cursor-pointer"
+                  onClick={() => onOptionSelect(option)}
+                >
+                  {option.name}
+                </button>
+              </li>
+            ))}
+          </ul>
+
         </div>
       </section>
 
---
+++ src/types/index.ts
@@ -0,0 +1,9 @@
+// içi je créée mon fivhier qui va gérer les types à l'aide de typescript
+// ça va me servir à faire appel à mes paramètre de mes datas 
+// et je vais m'en servir dans mon App.tsx
+
+export type optionType = {
+    name: string
+    lat:number
+    lon:number
+}
